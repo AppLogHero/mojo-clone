9090f22c690095a6fcf9026649aae73acb2180dd
AppLog <julien.delferiere@gmail.com>
2021-07-18 15:44:10 +0200

    push empty project

---
+++ mojo-clone/mojo_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  mojo_cloneApp.swift
+//  mojo-clone
+//
+//  Created by Julien Delferiere on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct mojo_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
